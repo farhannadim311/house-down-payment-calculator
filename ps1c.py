34e85c87d2d161f7f5445d78900965d41a19f847
Farhan Nadim Haque <farhannadimhaque@Farhans-Mini.lan>
2025-12-10 07:06:16 -0600

    done

--- ps1c.py
+++ ps1c.py
@@ -6,14 +6,49 @@
 ##############################################
 ## Get user input for initial_deposit below ##
 ##############################################
-
+initial_deposit = float(input("Enter the inital depost: "))
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
+cost_of_house = 800000
+down_payment = 0.25
+required_payment = cost_of_house * down_payment
+epsilon = 100
+time = 36
+low = 0
+high = 1
+r = (low + high) / 2
+steps = 1
+found = True
 
 
 ##################################################################################################
 ## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 ##################################################################################################
+while(True):
+    amount_saved = initial_deposit
+    for m in range(time):
+        amount_saved = (amount_saved * (1 + r / 12))
+    
+    if(abs(amount_saved - required_payment) <= epsilon):
+        break
+    if(low >= high):
+        found = False
+        r = None
+        steps = 0
+        break
+    elif(amount_saved > required_payment):
+        high = r
+    else:
+        low = r
+    r = (low + high) / 2
+    steps +=1
+if(found):
+    print(f"Best savings rate: {r}")
+    print(f"Steps in bisection search: {steps}")
+else:
+    print(f"Best stavings rate: {r}")
+    print(f"Steps in bisection search: {steps}")
+
 
--- ps1c_in_function.py
+++ ps1c_in_function.py
@@ -1,9 +1,45 @@
 def part_c(initial_deposit):
-	#########################################################################
-	
-	
-	##################################################################################################
-	## Determine the lowest rate of return needed to get the down payment for your dream home below ##
-	##################################################################################################
-	
-	return r, steps
+    ##############################################
+    ## Your original code starts here unchanged ##
+    ##############################################
+
+    cost_of_house = 800000
+    down_payment = 0.25
+    required_payment = cost_of_house * down_payment
+    epsilon = 100
+    time = 36
+    low = 0
+    high = 1
+    r = (low + high) / 2
+    steps = 1
+    found = True
+
+    while(True):
+        amount_saved = initial_deposit
+        for m in range(time):
+            amount_saved = (amount_saved * (1 + r / 12))
+        
+        if(abs(amount_saved - required_payment) <= epsilon):
+            break
+        if(low >= high):
+            found = False
+            steps = 0
+            r = None
+            break
+            
+        elif(amount_saved > required_payment):
+            high = r
+        else:
+            low = r
+
+        r = (low + high) / 2
+        steps += 1
+
+    if(found):
+        print(f"Best savings rate: {r}")
+        print(f"Steps in bisection search: {steps}")
+    else:
+        print(f"Best stavings rate: None")
+        print(f"Steps in bisection search: 0")
+
+    return r, steps
